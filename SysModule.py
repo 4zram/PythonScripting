545c19d928c72ae8c3c3f4430c5c547737b9bb9b
Selina <selina@gomez.com>
2021-12-07 21:51:33 +0530

    Initial commit

---
+++ SysModule.py
@@ -0,0 +1,10 @@
+"""
+Sys module is used to interact or manipulate the interpreter.
+It basically is related to everything with the system.
+"""
+import sys
+
+print(sys.version)
+print(sys.argv)
+print(sys.stdin)
+print(sys.stdout)
